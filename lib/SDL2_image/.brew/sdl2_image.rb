fdc63e4f02749bfe7d3309c58989451dfe0e2a72
Pierre Haudebourt <pierre.haudebourt@etu.unistra.fr>
2025-04-03 17:28:18 +0200

    Ajout SDL_image

---
+++ lib/SDL2_image/.brew/sdl2_image.rb
@@ -0,0 +1,58 @@
+class Sdl2Image < Formula
+  desc "Library for loading images as SDL surfaces and textures"
+  homepage "https://github.com/libsdl-org/SDL_image"
+  url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.8/SDL2_image-2.8.8.tar.gz"
+  sha256 "2213b56fdaff2220d0e38c8e420cbe1a83c87374190cba8c70af2156097ce30a"
+  license "Zlib"
+  head "https://github.com/libsdl-org/SDL_image.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^(?:release[._-])?v?(2(?:\.\d+)+)$/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkgconf" => :build
+  depends_on "jpeg-turbo"
+  depends_on "jpeg-xl"
+  depends_on "libavif"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "sdl2"
+  depends_on "webp"
+
+  def install
+    inreplace "SDL2_image.pc.in", "@prefix@", HOMEBREW_PREFIX
+
+    # upstream bug report, https://github.com/libsdl-org/SDL_image/issues/490
+    system "./autogen.sh"
+    system "./configure", "--disable-imageio",
+                          "--disable-avif-shared",
+                          "--disable-jpg-shared",
+                          "--disable-jxl-shared",
+                          "--disable-png-shared",
+                          "--disable-stb-image",
+                          "--disable-tif-shared",
+                          "--disable-webp-shared",
+                          *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <SDL2/SDL_image.h>
+
+      int main()
+      {
+          int INIT_FLAGS = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP | IMG_INIT_JXL | IMG_INIT_AVIF;
+          int result = IMG_Init(INIT_FLAGS);
+          IMG_Quit();
+          return result == INIT_FLAGS ? EXIT_SUCCESS : EXIT_FAILURE;
+      }
+    C
+    system ENV.cc, "test.c", "-I#{Formula["sdl2"].opt_include}/SDL2", "-L#{lib}", "-lSDL2_image", "-o", "test"
+    system "./test"
+  end
+end
