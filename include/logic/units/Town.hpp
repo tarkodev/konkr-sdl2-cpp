f6ef6a6ecac63f795920f8baa8549365536d7c40
Pierre Haudebourt <pierre.haudebourt@etu.unistra.fr>
2025-04-28 17:42:59 +0200

    Ajout Halo town

--- include/logic/units/Town.hpp
+++ include/logic/units/Town.hpp
@@ -11,15 +11,20 @@ public:
 
     explicit Town(Player* owner = nullptr);
 
-    void onTurnStart() override;                  ///< génère l’or du province
+    void onTurnStart() override;
     void display(const Texture* target, const Point& pos) override;
     const Size getSize() const override { return sprite_->getSize(); }
+
+    void setSelected(bool selected) { selected_ = selected; }
 
     /* Chargement des ressources partagées */
     static void init();
 
 private:
     static Texture* sprite_;
+    static Texture* selectSprite_;
+
+    bool selected_ = true;
 };
 
 #endif
--- src/logic/units/Town.cpp
+++ src/logic/units/Town.cpp
@@ -2,6 +2,17 @@
 #include "Texture.hpp"
 
 Texture* Town::sprite_ = nullptr;
+Texture* Town::selectSprite_ = nullptr;
+
+void Town::init()
+{
+    if (!renderer)
+        std::runtime_error("GameElement not initialized");
+
+    if (sprite_) return;
+    sprite_ = (new Texture(renderer, "../assets/img/town.png"))->convertAlpha();
+    selectSprite_ = (new Texture(renderer, "../assets/img/bgtown.png"))->convertAlpha();
+}
 
 Town::Town(Player* owner)
     : Building(STRENGTH, COST, UPKEEP, owner) {}
@@ -14,15 +25,9 @@ void Town::onTurnStart()
 void Town::display(const Texture* target, const Point& pos)
 {
     if (!sprite_) return;
-    target->blit(sprite_, Point{pos.getX()-sprite_->getWidth()/2,
-                                pos.getY()-sprite_->getHeight()/2});
-}
 
-void Town::init()
-{
-    if (!renderer)
-        std::runtime_error("GameElement not initialized");
+    if (selected_)
+        target->blit(selectSprite_, Point{pos.getX()-selectSprite_->getWidth()/2, pos.getY()-selectSprite_->getHeight()/2});
 
-    if (sprite_) return;
-    sprite_ = new Texture(renderer, "../assets/img/town.png");
+    target->blit(sprite_, Point{pos.getX()-sprite_->getWidth()/2, pos.getY()-sprite_->getHeight()/2});
 }
